bc304690e24eaa6107edc64815c0b14804235d93
ojeks31 <seun.ojekunle@gmail.com>
2023-02-17 17:54:39 -0800

    Write a program that prints the alphabet in lowercase,

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Return:Always 0
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
-{	
-       	char ch;
-	for (ch = 'a' ; ch <= 'z' ; ch++)
 {
+	char letter;
 
-	putchar(ch);
-	putchar(‘\n’);
-}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
 	return (0);
 }
-
 
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -5,3 +5,4 @@ a program that prints all the numbers of base 16 in lowercase
 a program that prints all single digit numbers of base 10 starting from 0
 Write a program that prints the alphabet in lowercase, followed by a new line.
 Write a program that prints the alphabet in lowercase, and then in uppercase
+Write a program that prints the alphabet in lowercase,
