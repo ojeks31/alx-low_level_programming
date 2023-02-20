5e99eaf19ea20581efc7a2b6583763552f04b1ef
ojeks31 <seun.ojekunle@gmail.com>
2023-02-20 01:11:14 -0800

    differentt size type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	printf("size of a char: %ld\n", sizeof(char));
+	printf("size of a char: %ld byte(s)\n", sizeof(char));
 	printf("size of an int: %ld byte(s)\n", sizeof(int));
 	printf("size of a long int: %ld byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %ld byte(s)\n", sizeof(long long int));
