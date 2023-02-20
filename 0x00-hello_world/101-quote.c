0826179ff7eefcd0f862fdad4d79cc576ce81748
ojeks31 <seun.ojekunle@gmail.com>
2023-02-20 00:41:43 -0800

    differentt size type

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 on success
+*/
+
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19'");
+
+	return (1);
+}
+
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-	printf("size of data char: %lu/n", sizeof(char));
-	printf("size of data int: %lu byte(s)/n", sizeof(int));
-	printf("size of data long int: %lu byte(s)/n", sizeof(long int));
-	printf("size of data char: %lu byte(s)/n", sizeof(long long int));
-	printf("size of data float: %lu byte(s)/n", sizeof(float));
+	printf("size of data char: %lu\n", sizeof(char));
+	printf("size of data int: %lu byte(s)\n", sizeof(int));
+	printf("size of data long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of data char: %lu byte(s)\n", sizeof(long long int));
+	printf("size of data float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
 
