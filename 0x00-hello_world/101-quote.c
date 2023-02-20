0abe096358a912a97bd91f64f1403c23ee5fc263
ojeks31 <seun.ojekunle@gmail.com>
2023-02-20 02:28:10 -0800

    prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - entry point
- * Return: 0 on success
-*/
+ * main - prints exactly "and that piece of art is useful"
+ * "- Dora Korpar, 2015-10-19"
+ * followed by a new line, to the standard error.
+ * Return: Alway 1
+ */
 
 int main(void)
 {
-	system("echo 'and that piece of art is useful\" \
-- Dora Korpar, 2015-10-19'");
-
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-
