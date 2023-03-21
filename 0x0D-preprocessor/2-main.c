de98bcea734831f19adc78610aa2605c05835642
ojeks31 <seun.ojekunle@gmail.com>
2023-03-20 20:17:13 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ * Description: print the name of the file
+ * Return Always 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", __file__);
+	return (0);
+}
