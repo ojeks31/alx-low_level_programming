a2c38fdb49078c62a05345dfd72c1b800ac1a24a
ojeks31 <seun.ojekunle@gmail.com>
2023-04-04 15:56:30 -0700

    frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2.c - frees a listint_t list
+ * @head: contains the address of the first node
+ * Return: Always 0
+ * Description: singly linked list that fress a list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *last;
+
+	while ((*head) != NULL)
+	{
+		last = (*head);
+		(*head) = NULL;
+		free(last);
+	}
+}
